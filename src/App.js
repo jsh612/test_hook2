e726110e17ae907fbb45c3da6d73e59b77b6c2d2
seungho Jeon <urijsh612@gmail.com>
2020-01-10 20:39:28 +0900

    #9.6 Understanding useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import Screen from "./Screen";
-import Lang from "./context";
-import translations from "./translations";
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  console.log("state", state);
+  console.log("action", action);
+  // 리턴 값 = 새로운 state가 된다.(기존 state가 대체됨)
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  // dispatch: reducer에게 입력된 매개변수를 전달 + reducer 함수 실행
+  // reducer : dispatch의 입력값에 따라 기존 state를 가공하여 새로운 state를 출력하는 역할
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
-    <Lang defaultLang="en" translations={translations}>
-      <Screen />
-    </Lang>
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+    </>
   );
 }
 
