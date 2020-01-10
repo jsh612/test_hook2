b480bc84f1cdade592a4a9f8005991aeb6056f0b
seungho Jeon <urijsh612@gmail.com>
2020-01-10 22:34:35 +0900

    #9.8 Adding To Dos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+const initialState = { toDos: [] };
+const ADD = "add";
 
 const reducer = (state, action) => {
-  console.log("state", state);
-  console.log("action", action);
-  // 리턴 값 = 새로운 state가 된다.(기존 state가 대체됨)
+  // → action : dispatch에서 쏴준값들이 들어간다
   switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+    case ADD:
+      return { toDos: [...state.toDos, { text: action.payload }] };
     default:
-      throw new Error();
+      return;
   }
 };
 
 function App() {
   // dispatch: reducer에게 입력된 매개변수를 전달 + reducer 함수 실행
   // reducer : dispatch의 입력값에 따라 기존 state를 가공하여 새로운 state를 출력하는 역할
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const [state, dispatch] = useReducer(reducer, initialState); // 데이터 누적용도
+
+  const [newToDo, setNewToDo] = useState(""); // 현재입력값 저장 용도
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+      <h2>Add to do</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+
+      <ul>
+        <h2>할 일</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={index}>{toDo.text}</li>
+        ))}
+      </ul>
     </>
   );
 }
