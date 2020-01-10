3757657d3e9cc2327d0c69d17eaa7f447f2fefa4
seungho Jeon <urijsh612@gmail.com>
2020-01-10 22:54:23 +0900

    #9.9 Deleting To Dos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useReducer, useState } from "react";
+import uuid from "uuid/v4";
 
-const initialState = { toDos: [] };
 const ADD = "add";
+const DEL = "del";
 
 const reducer = (state, action) => {
   // → action : dispatch에서 쏴준값들이 들어간다
+
   switch (action.type) {
     case ADD:
-      return { toDos: [...state.toDos, { text: action.payload }] };
+      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+    case DEL:
+      return { toDos: state.toDos.filter(toDo => toDo.id !== action.payload) };
     default:
       return;
   }
 };
 
 function App() {
-  // dispatch: reducer에게 입력된 매개변수를 전달 + reducer 함수 실행
+  const initialState = { toDos: [] };
+  // dispatch: reducer에게 입력된 매개변수를 전달(reducer의 action에 할당됨) + reducer 함수 실행
   // reducer : dispatch의 입력값에 따라 기존 state를 가공하여 새로운 state를 출력하는 역할
 
   const [state, dispatch] = useReducer(reducer, initialState); // 데이터 누적용도
@@ -47,7 +52,12 @@ function App() {
       <ul>
         <h2>할 일</h2>
         {state.toDos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
+          <li key={index}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              ❌
+            </button>
+          </li>
         ))}
       </ul>
     </>
